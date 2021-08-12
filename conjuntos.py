028f2970e4f2fe75584da052dbef928efd411618
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-08-12 19:51:18 -0300

    Adicionando metodo construtor

--- conjuntos.py
+++ conjuntos.py
@@ -1,2 +1,10 @@
 class Conjunto:
-    pass
+    def __init__(self, *args):
+        if type(args[0]) == list:
+            self.elementos = tuple(args[0])
+        elif type(args[0]) == Conjunto:
+            self.elementos = args[0].elementos    
+        else:
+            self.elementos = args
+            
+    
