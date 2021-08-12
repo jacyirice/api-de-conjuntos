93876707eccfb0dab64cebd1d8d7c17494b0d18e
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-08-12 20:23:07 -0300

    Adicionando metodo contem

--- conjuntos.py
+++ conjuntos.py
@@ -13,3 +13,11 @@ class Conjunto:
     def possui(self, e):
         return e in self.elementos
     
+    def contem(self, b):
+        flag = True
+        for i in b.elementos:
+            if i not in self.elementos:
+                flag = False
+                break
+            
+        return flag 
